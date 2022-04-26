5979c1abd321b198de02887e176ea4e58feffae9
ThanhLe <thanhlesy.devplus@gmail.com>
2022-04-26 21:21:27 +0700

    :sparkles: List Profile By Campaign :sparkles:

--- src/shared/reducers/index.js
+++ src/shared/reducers/index.js
@@ -1,8 +1,10 @@
 import { combineReducers } from "redux";
 import campaigns from "./campaignReducer";
 import alert from "./alertReducer";
+import profiles from "./profileReducer";
 
 export default combineReducers({
   campaigns,
   alert,
+  profiles,
 });
